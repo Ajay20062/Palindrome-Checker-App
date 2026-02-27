001c32ee76192c566460b04dd789e84b4719eda9
T R Ajay Dharrsan <70842623+Ajay20062@users.noreply.github.com>
2026-02-27 10:42:33 +0530

    Update version number to 3.0 in PalindromeCheckerApp

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -9,7 +9,7 @@ import java.util.*;
  * Now accepts input from user.
  *</p>
  * @author T R Ajay Dharrsan
- * @version 1.0
+ * @version 3.0
  */
 
 public class PalindromeCheckerApp {
@@ -23,7 +23,7 @@ public class PalindromeCheckerApp {
         System.out.println("=======================================================");
         System.out.println();
         System.out.println("=======================================================");
-        System.out.println("                 Version: 1.0                 ");
+        System.out.println("                 Version: 3.0                 ");
         System.out.println("     Welcome to Palindrome Application!       ");
         System.out.println("       Application started successfully.      ");
         System.out.println("========================================================");
@@ -35,6 +35,7 @@ public class PalindromeCheckerApp {
 
         System.out.println("UC2 - Half Loop                 : " + checkHalfLoop(input));
         System.out.println("UC3 - Reverse String            : " + checkReverse(input));
+        scanner.close();
     }
 
     // UC2 - Half Loop
