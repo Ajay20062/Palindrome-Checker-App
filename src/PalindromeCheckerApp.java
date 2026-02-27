1aa4a6c2cae9ce51b0c338a773b88e4093586069
Ajay20062 <ajaydharrsan@gmail.com>
2026-02-27 09:49:23 +0530

    UC1: Add welcome message module

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+/**
+ * =====================================================
+ * MAIN CLASS - Palindrome Checker App
+ * =====================================================
+ * <p>
+ * All palindrome validation techniques combined.
+ * Now accepts input from user.
+ *</p>
+ * @author T R Ajay Dharrsan
+ * @version 1.0
+ */
+
+public class PalindromeCheckerApp {
+
+    static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=======================================================");
+        System.out.println("               PALINDROME CHECKER App         ");
+        System.out.println("=======================================================");
+        System.out.println();
+        System.out.println("=======================================================");
+        System.out.println("                 Version: 9.0                 ");
+        System.out.println("     Welcome to Palindrome Application!       ");
+        System.out.println("       Application started successfully.      ");
+        System.out.println("========================================================");
+        System.out.println();
+        scanner.close();
+    }
+}
