593b0556be1d77b97bc23dff31ff01a836e6933c
T R Ajay Dharrsan <70842623+Ajay20062@users.noreply.github.com>
2026-03-07 21:51:23 +0530

    Update version number in PalindromeCheckerApp

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -23,7 +23,7 @@ public class PalindromeCheckerApp {
         System.out.println("=======================================================");
         System.out.println();
         System.out.println("=======================================================");
-        System.out.println("                 Version: 9.0                 ");
+        System.out.println("                 Version: 2.0                 ");
         System.out.println("     Welcome to Palindrome Application!       ");
         System.out.println("       Application started successfully.      ");
         System.out.println("========================================================");
