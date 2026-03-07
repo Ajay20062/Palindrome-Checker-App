8fbc0f87e84a52724aaa26ab0fed1651f3d8ef76
Ajay20062 <70842623+Ajay20062@users.noreply.github.com>
2026-03-07 21:50:22 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 /**
  * =====================================================
@@ -8,22 +9,43 @@
  * Now accepts input from user.
  *</p>
  * @author T R Ajay Dharrsan
- * @version 1.0
+ * @version 2.0
  */
 
 public class PalindromeCheckerApp {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("=======================================================");
         System.out.println("               PALINDROME CHECKER App         ");
         System.out.println("=======================================================");
         System.out.println();
         System.out.println("=======================================================");
-        System.out.println("                 Version: 1.0                 ");
+        System.out.println("                 Version: 9.0                 ");
         System.out.println("     Welcome to Palindrome Application!       ");
         System.out.println("       Application started successfully.      ");
         System.out.println("========================================================");
         System.out.println();
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        System.out.println("========================================================");
+
+        System.out.println("UC2 - Half Loop            : " + checkHalfLoop(input));
+
+        scanner.close();
+    }
+
+    // UC2 - Half Loop
+    public static boolean checkHalfLoop(String input) {
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
